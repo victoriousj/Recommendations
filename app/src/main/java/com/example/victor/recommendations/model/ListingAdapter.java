9792f549d9bb003c7e35ce1c5c049dffb949ecd9
VictoriousJ <victordjohnson@gmail.com>
2018-05-01 20:22:45 -0500

    expanded on the Holder for the list adapter

--- app/src/main/java/com/example/victor/recommendations/model/ListingAdapter.java
+++ app/src/main/java/com/example/victor/recommendations/model/ListingAdapter.java
@@ -1,14 +1,27 @@
 package com.example.victor.recommendations.model;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import com.example.victor.recommendations.R;
 
 public class ListingAdapter extends RecyclerView.Adapter<ListingAdapter.ListingHolder> {
 
+    private LayoutInflater inflater;
+
+    public ListingAdapter(Context context) {
+        inflater = LayoutInflater.from(context);
+    }
+
     @Override
     public ListingHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        return new ListingHolder(inflater.inflate(R.layout.layout_listing, parent, false));
     }
 
     @Override
@@ -23,8 +36,18 @@ public class ListingAdapter extends RecyclerView.Adapter<ListingAdapter.ListingH
 
     public class ListingHolder extends RecyclerView.ViewHolder {
 
+        public ImageView mImageView;
+        public TextView titleView;
+        public TextView shopNameView;
+        public TextView priceView;
+
         public ListingHolder(View itemView) {
             super(itemView);
+
+            mImageView = itemView.findViewById(R.id.listing_image);
+            titleView = itemView.findViewById(R.id.listing_title);
+            shopNameView = itemView.findViewById(R.id.listing_shop_name);
+            priceView = itemView.findViewById(R.id.listing_price);
         }
     }
 }
